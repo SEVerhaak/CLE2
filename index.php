decaa6d9f8a6ff228d906633dbb5e38c76b96a09
SEVerhaak <145699852+SEVerhaak@users.noreply.github.com>
2024-01-10 12:47:53 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Denise Kookt</title>
+</head>
+<header>
+    <nav>
+        <div class="nav-right">
+            <img class="logo" src="">
+            <a class="header-link-text" href="#">Reserveren</a>
+            <a class="header-link-text" href="#">Over ons</a>
+            <a class="header-link-text" href="#">Nieuws</a>
+            <a class="header-link-text" href="#">Contact</a>
+        </div>
+        <div class="nav-left">
+            <a class="login">Login</a>
+        </div>
+    </nav>
+</header>
+<body>
+
+</body>
+<footer>
+    <img class="logo" src="">
+    <p class="footer-main-text">Denise Kookt</p>
+    <div class="socials">
+        <p class="footer-social-text">Socials</p>
+        <img class="social-img" src="">
+        <img class="social-img" src="">
+    </div>
+</footer>
+</html>
