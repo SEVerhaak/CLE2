562918c975a68221e5155cdc36778935ea87a2ce
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-01-11 14:17:37 +0100

    Calendar added Admin.css added Style.css edited

--- admin.php
+++ admin.php
@@ -23,6 +23,7 @@ mysqli_close($db);
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/admin.css">
     <title>Denise Kookt!</title>
 </head>
 <header>
@@ -40,6 +41,7 @@ mysqli_close($db);
     </nav>
 </header>
 <body>
+
 <div class="table">
 <table border="1">
     <thead>
@@ -62,6 +64,59 @@ mysqli_close($db);
     </tbody>
 </table>
 </div>
+
+<div class="month">
+    <ul>
+        <li class="prev">&#10094;</li>
+        <li class="next">&#10095;</li>
+        <li>January<br><span style="font-size:18px">2024</span></li>
+    </ul>
+</div>
+
+<ul class="weekdays">
+    <li>Mo</li>
+    <li>Tu</li>
+    <li>We</li>
+    <li>Th</li>
+    <li>Fr</li>
+    <li>Sa</li>
+    <li>Su</li>
+</ul>
+
+<ul class="days">
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+    <li>6</li>
+    <li>7</li>
+    <li>8</li>
+    <li>9</li>
+    <li><span class="active">10</span></li>
+    <li>11</li>
+    <li>12</li>
+    <li>13</li>
+    <li>14</li>
+    <li>15</li>
+    <li>16</li>
+    <li>17</li>
+    <li>18</li>
+    <li>19</li>
+    <li>20</li>
+    <li>21</li>
+    <li>22</li>
+    <li>23</li>
+    <li>24</li>
+    <li>25</li>
+    <li>26</li>
+    <li>27</li>
+    <li>28</li>
+    <li>29</li>
+    <li>30</li>
+    <li>31</li>
+</ul>
+
 </body>
 <footer>
     <div class = "footer-style">
