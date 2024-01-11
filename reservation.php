119384c06c895711f885fa35754c01c6e55f6846
SEVerhaak <severhaak@hotmail.nl>
2024-01-11 13:32:42 +0100

    dit fixt alles

--- reservation.php
+++ reservation.php
@@ -26,37 +26,45 @@
     </nav>
 </header>
 <body>
-    <div class = "reservation-box">
-        <h1>Wat wil je boeken</h1>
-        <form action = "#" method="POST">
-            <input type = "radio" name = "service" id = "catering">
-                <img class = "catering">
-                <label for = "service1"> Catering </label>
-            <input type = "radio" name = "service" id = "workshop">
-                <img class = "workshop">
-                <label for = "service2"> Workshop </label>
-            <label for = "date" >Voor welke datum?</label>
-            <input type = "date" name = "date" id = "date">
-            <div class = "available-time">
-                <p>16:00-17:00</p>
-                <p>prijs</p>
-                <input type = "radio" name = "time" id = "time">
-            </div>
-            <div class = "available-time">
-                <p>17:00-18:00</p>
-                <p>prijs</p>
-                <input type = "radio" name = "time" id = "time">
-            </div>
-            <div class = "available-time">
-                <p>18:00-19:00</p>
-                <p>prijs</p>
-                <input type = "radio" name = "time" id = "time">
-            </div>
-            <input type = "number" name = "amount_people" min = "2" max = "16">
-            <label for = "amount_people">Amount of people</label>
-            <button type = "submit">Reserveren!</button>
-        </form>
-    </div>
+<div class="reservation-box">
+    <h1>Wat wil je boeken</h1>
+    <form action="#" method="POST">
+        <div class="flex-side">
+            <input type="radio" name="service" id="catering">
+            <img class="catering">
+            <label for="service1"> Catering </label>
+            <input type="radio" name="service" id="workshop">
+            <img class="workshop">
+            <label for="service2"> Workshop </label>
+        </div>
+        <div class="flex-down">
+            <label for="date">Voor welke datum?</label>
+            <input type="date" name="date" id="date">
+        </div>
+        <div class="flex-side">
+            <label for="amount_people">Amount of people</label>
+            <button type="button" class="left-button" id="left-button-id">-</button>
+            <input class="amount-value" type="number" value="2" name="amount_people" min="2" max="16">
+            <button type="button" class="right-button" id="right-button-id">+</button>
+        </div>
+        <div class="available-time">
+            <p>16:00-17:00</p>
+            <p>prijs</p>
+            <input type="radio" name="time" id="time">
+        </div>
+        <div class="available-time">
+            <p>17:00-18:00</p>
+            <p>prijs</p>
+            <input type="radio" name="time" id="time">
+        </div>
+        <div class="available-time">
+            <p>18:00-19:00</p>
+            <p>prijs</p>
+            <input type="radio" name="time" id="time">
+        </div>
+        <button type="submit">Reserveren!</button>
+    </form>
+</div>
 </body>
 <footer>
     <div class = "footer-style">
@@ -66,6 +74,33 @@
         <!-- hoi -->
     </div>
 </footer>
+<script>
+    let element = document.getElementsByClassName('amount-value')[0];
+    let leftButton = document.getElementsByClassName('left-button')[0];
+    let rightButton = document.getElementsByClassName('right-button')[0];
+    leftButton.addEventListener("click", decrease);
+    rightButton.addEventListener("click", increase);
+
+    function increase() {
+        let value = parseInt(element.value)
+        if (value >= 16) {
+       } else {
+            element.value = value + 1;
+        }
+    }
+
+    function decrease() {
+        let value = parseInt(element.value)
+        if (value <= 2){
+
+        } else{
+            element.value = value - 1;
+        }
+
+
+    }
+
+</script>
 </html>
 <div class="socials">
 
