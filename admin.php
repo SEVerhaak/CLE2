9941db357faf6511257bed9f9f450c702febd3bb
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-01-11 13:42:07 +0100

    kaas

--- admin.php
+++ admin.php
@@ -22,24 +22,25 @@ mysqli_close($db);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
     <title>Denise Kookt!</title>
 </head>
 <header>
     <nav>
         <div class="nav-right">
-            <img class="logo" src="">
+            <img class="logo" src="img/logo_denisekookt.png">
             <a class="header-link-text" href="#">Reserveren</a>
             <a class="header-link-text" href="#">Over ons</a>
             <a class="header-link-text" href="#">Nieuws</a>
             <a class="header-link-text" href="#">Contact</a>
         </div>
         <div class="nav-left">
-            <a class="login">Login</a>
+            <a class="login" href = "#">Login</a>
         </div>
     </nav>
 </header>
 <body>
-
+<div class="table">
 <table border="1">
     <thead>
     <tr>
@@ -60,15 +61,13 @@ mysqli_close($db);
     <?php endforeach; ?>
     </tbody>
 </table>
-
+</div>
 </body>
 <footer>
-    <img class="logo" src="">
-    <p class="footer-main-text">Denise Kookt</p>
-    <div class="socials">
+    <div class = "footer-style">
+        <img class="logo" src="img/logo_denisekookt.png">
+        <p class="footer-main-text">Denise Kookt!</p>
         <p class="footer-social-text">Socials</p>
-        <img class="social-img" src="">
-        <img class="social-img" src="">
     </div>
 </footer>
 </html>
