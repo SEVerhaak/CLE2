3578a3bf6479330eb97349c7f9f2746b0840c4ea
ElisaZornig <elisazornig@gmail.com>
2024-01-15 14:49:00 +0100

    Css reserveringspagina

--- admin.php
+++ admin.php
@@ -25,7 +25,7 @@ mysqli_close($db);
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="css/style.css" rel="stylesheet">
-    <link href="css/admin.css" rel="stylesheet" >
+    <link href="css/admin.css" rel="stylesheet">
     <link href="css/example.css" rel="stylesheet">
     <link href="css/calendar.css" rel="stylesheet">
     <title>Denise Kookt!</title>
@@ -46,48 +46,57 @@ mysqli_close($db);
     </nav>
 </header>
 <body>
-<!-- Tabel met reserveringen uit de Database -->
-<div class="table">
-    <table border="1">
-        <thead>
-        <tr>
-            <?php
-            foreach (array_keys($reservations[0]) as $columnName) {
-                echo '<th>' . $columnName . '</th>';
-            }
-            ?>
-        </tr>
-        </thead>
-        <tbody>
-        <?php foreach ($reservations as $reservation): ?>
+<div class="sidebar">
+    <a href="#home"><img src = "img/home.png"></a>
+    <a href="#mail"><img src = "img/mail.png"></a>
+    <a href="#calender"><img src = "img/agenda.png"></a>
+    <a href="#money"><img src = "img/dollar.png"></a>
+    <a href="#settings"><img src = "img/settings.png"></a>
+</div>
+<div class="admin-box">
+    <!-- Tabel met reserveringen uit de Database -->
+    <div class="table">
+        <table border="1">
+            <thead>
             <tr>
-                <?php foreach ($reservation as $value): ?>
-                    <td><?php echo $value; ?></td>
-                <?php endforeach; ?>
+                <?php
+                foreach (array_keys($reservations[0]) as $columnName) {
+                    echo '<th>' . $columnName . '</th>';
+                }
+                ?>
             </tr>
-        <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+            <?php foreach ($reservations as $reservation): ?>
+                <tr>
+                    <?php foreach ($reservation as $value): ?>
+                        <td><?php echo $value; ?></td>
+                    <?php endforeach; ?>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <!-- Evenementen aan de kalender toevoegen (moet nog verbonden worden met de database) -->
+    <?php
+    include 'Calendar.php';
+    $calendar = new Calendar();
+    $calendar->add_event('Workshop 18:00', '2024-01-03', 1, 'green');
+    $calendar->add_event('Catering 17:00', '2024-01-07', 1, 'yellow');
+    $calendar->add_event('Workshop 16:30', '2024-01-23', 1, 'green');
+    $calendar->add_event('Catering 17:30', '2024-01-31', 1, 'yellow');
+    ?>
+    <!-- Kalender met navigatieknoppen -->
+    <div class="content home">
+        <a href="?month=<?= $calendar->getPrevMonth() ?>" class="linkOne">Previous Month</a>
+        <a href="?month=<?= $calendar->getNextMonth() ?>" class="linkTwo">Next Month</a>
+        <?= $calendar ?>
+    </div>
+    <!-- Footer -->
 </div>
-<!-- Evenementen aan de kalender toevoegen (moet nog verbonden worden met de database) -->
-<?php
-include 'Calendar.php';
-$calendar = new Calendar();
-$calendar->add_event('Workshop 18:00', '2024-01-03', 1, 'green');
-$calendar->add_event('Catering 17:00', '2024-01-07', 1, 'yellow');
-$calendar->add_event('Workshop 16:30', '2024-01-23', 1, 'green');
-$calendar->add_event('Catering 17:30', '2024-01-31', 1, 'yellow');
-?>
-<!-- Kalender met navigatieknoppen -->
-<div class="content home">
-    <a href="?month=<?= $calendar->getPrevMonth() ?>" class="linkOne">Previous Month</a>
-    <a href="?month=<?= $calendar->getNextMonth() ?>" class="linkTwo">Next Month</a>
-    <?=$calendar?>
-</div>
-<!-- Footer -->
 </body>
 <footer>
-    <div class = "footer-style">
+    <div class="footer-style">
         <img class="logo" src="img/logo_dk.png">
         <p class="footer-main-text">Denise Kookt!</p>
         <p class="footer-social-text">Socials</p>
