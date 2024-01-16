40d0188ff3fb76ae710ce9968d9e73a3c6a44f37
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-01-16 10:20:43 +0100

    About.php added

---
+++ about.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="css/style.css" rel="stylesheet">
+    <title>Denise Kookt!</title>
+</head>
+<header>
+    <nav>
+        <div class="nav-right">
+            <img class="logo" src="img/logo_dk.png">
+            <a class="header-link-text" href="#">Reserveren</a>
+            <a class="header-link-text" href="#">Over ons</a>
+            <a class="header-link-text" href="#">Nieuws</a>
+            <a class="header-link-text" href="#">Contact</a>
+        </div>
+        <div class="nav-left">
+            <a class="login" href="admin.php">Login</a>
+        </div>
+    </nav>
+</header>
+<body>
+
+</body>
+<footer>
+    <div class="footer-style">
+        <img class="logo" src="img/logo_dk.png">
+        <p class="footer-main-text">Denise Kookt!</p>
+        <p class="footer-social-text">Socials</p>
+    </div>
+</footer>
+</html>
