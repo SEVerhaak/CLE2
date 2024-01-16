091febfa8534ff3f89cb2a2d1086dbc4eaecf915
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-01-16 10:49:59 +0100

    Header edited

--- admin.php
+++ admin.php
@@ -35,8 +35,8 @@ mysqli_close($db);
     <nav>
         <div class="nav-right">
             <img class="logo" src="img/logo_dk.png">
-            <a class="header-link-text" href="#">Reserveren</a>
-            <a class="header-link-text" href="#">Over ons</a>
+            <a class="header-link-text" href="reservation.php">Reserveren</a>
+            <a class="header-link-text" href="about.php">Over ons</a>
             <a class="header-link-text" href="#">Nieuws</a>
             <a class="header-link-text" href="#">Contact</a>
         </div>
--- index.php
+++ index.php
@@ -14,8 +14,8 @@
     <nav>
         <div class="nav-right">
             <img class="logo" src="img/logo_dk.png">
-            <a class="header-link-text" href="#">Reserveren</a>
-            <a class="header-link-text" href="#">Over ons</a>
+            <a class="header-link-text" href="reservation.php">Reserveren</a>
+            <a class="header-link-text" href="about.php">Over ons</a>
             <a class="header-link-text" href="#">Nieuws</a>
             <a class="header-link-text" href="#">Contact</a>
         </div>
