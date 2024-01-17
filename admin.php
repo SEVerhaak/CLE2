fbc0f1fd6d2302b2c533187d3dd4775edff93d71
ElisaZornig <elisazornig@gmail.com>
2024-01-17 11:40:35 +0100

    header link in img

--- admin.php
+++ admin.php
@@ -39,11 +39,13 @@ mysqli_close($db);
 <header>
     <nav>
         <div class="nav-right">
-            <img class="logo" src="img/logo_dk.png">
+            <a href = "index.php"><img class="logo" src="img/logo_dk.png"></a>
+            <div class = "header-links">
             <a class="header-link-text" href="reservation.php">Reserveren</a>
             <a class="header-link-text" href="about.php">Over ons</a>
             <a class="header-link-text" href="news.php">Nieuws</a>
             <a class="header-link-text" href="contact.php">Contact</a>
+            </div>
         </div>
         <div class="nav-left">
             <a class="login" href="admin.php">Login</a>
--- reservationUser.php
+++ reservationUser.php
@@ -114,11 +114,13 @@ if (count($settings) === 0) {
 <header>
     <nav>
         <div class="nav-right">
-            <img class="logo" src="img/logo_dk.png">
-            <a class="header-link-text" href="reservation.php">Reserveren</a>
-            <a class="header-link-text" href="about.php">Over ons</a>
-            <a class="header-link-text" href="news.php">Nieuws</a>
-            <a class="header-link-text" href="contact.php">Contact</a>
+            <a href = "index.php"><img class="logo" src="img/logo_dk.png"></a>
+            <div class = "header-links">
+                <a class="header-link-text" href="reservation.php">Reserveren</a>
+                <a class="header-link-text" href="about.php">Over ons</a>
+                <a class="header-link-text" href="news.php">Nieuws</a>
+                <a class="header-link-text" href="contact.php">Contact</a>
+            </div>
         </div>
         <div class="nav-left">
             <a class="login" href="admin.php">Login</a>
