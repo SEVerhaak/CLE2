003e9d125dcdaa9907f1764a19a3ca1f44fdc259
Chris Tang <1073412@hr.nl>
2024-01-19 11:07:12 +0100

    test bestand verwijderen

--- alleenVoorChris/includes/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host       = "localhost";
-$database   = "CLE2";
-$user       = "root";
-$password   = "";
-
-$db = mysqli_connect($host, $user, $password, $database)
-or die("Error: " . mysqli_connect_error());
