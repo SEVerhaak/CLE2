3ed8b0a09ea62eef0b06215641c91e4b0ea10d9f
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-01-23 11:12:02 +0100

    about.php edited

--- about.php
+++ about.php
@@ -50,7 +50,7 @@ session_start();
 <main>
     <div class="block">
         <h1>Over Ons</h1>
-        <p>Denise Kookt! is een kookbedrijf gespecialiseerd in het organiseren van kookworkshops, het verzorgen van thuisrestaurants en het leveren van catering. Denise Kookt! is een zelfstandige onderneming van Denise. Via het online-reserveringssysteem zijn workshops en catering snel en eenvoudig te boeken.</p>
+        <p>Denise Kookt! is een kookbedrijf gespecialiseerd in het organiseren van kookworkshops, het verzorgen van thuisrestaurants en het leveren van catering. Denise Kookt! is een zelfstandige onderneming van Denise. Via het online-reserveringssysteem zijn workshops en catering snel en eenvoudig te boeken. Denise's diensten zijn beschikbaar in Breda en omstreken.</p>
         <img src="./img/denisekookt.jpg" alt="Denise" class="Denise">
     </div>
 </main>
